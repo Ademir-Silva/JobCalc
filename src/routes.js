a20b23f9f6426feadc80d58d2ade5abf6460117b
Ademir-Silva <ademirfernandessilva622@gmail.com>
2021-03-30 22:17:39 -0300

    Atualizado

--- src/routes.js
+++ src/routes.js
@@ -5,15 +5,47 @@ const views = __dirname + "/views/"
 
 const profile = {
     name: "Ademir Silva",
-    avatar: "https://avatars.githubusercontent.com/u/66567965?v=4",
+    avatar: "https://github.com/Ademir-Silva.png",
     "monthly-budget": 3000,
     "days-per-week": 5,
     "hours-per-day": 5,
     "vacation-per-year": 4
 }
 
-routes.get('/', (req, res) => res.render(views + "index"))
+const jobs = [
+    {
+        id: 1,
+        name: "Pizzaria Guloso",
+        "daily-hours": 2,
+        "total-hours": 60,
+        created_at: Date.now(),
+    },
+
+    {
+        id: 2,
+        name: "OneTwo Project",
+        "daily-hours": 3,
+        "total-hours": 47,
+        created_at: Date.now()
+    },
+]
+
+routes.get('/', (req, res) => res.render(views + "index", {jobs}))
 routes.get('/job', (req, res) => res.render(views + "job"))
+routes.post('/job', (req, res) => {
+
+    const lastId = jobs[jobs.length - 1]?.id || 1
+
+    jobs.push({
+        job: lastId + 1,
+        name: req.body.name,
+        "daily-hours": req.body["daily-hours"],
+        "total-hours": req.body["total-hours"],
+        created_at: Date.now()
+    })
+
+    return res.redirect('/')
+})
 routes.get('/jobedit', (req, res) => res.render(views + "job-edit"))
 routes.get('/profile', (req, res) => res.render(views + "profile", { profile }))
 
